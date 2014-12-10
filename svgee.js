6956dcfae4b5ae20041aedf7554e23c7d2378be8
James Haley <jlhaley1@gmail.com>
2014-12-09 22:18:48 -0800

    Add SVG line element

--- svgee.js
+++ svgee.js
@@ -104,8 +104,28 @@
    */
   function square(x, y, sideLength) {
     // delegate to rect function
-    console.log(x, y, sideLength);
     return rect(x, y, sideLength, sideLength);
+  }
+
+  /**
+   * Creates an SVG line element.
+   *
+   * @param {number} x1 The x-coordinate of the first point.
+   * @param {number} y1 The y-coordiante of the first point.
+   * @param {number} x2 The x-coordinate of the second point.
+   * @param {number} y2 The y-coordinate of the second point.
+   *
+   * @returns The SVG line element to be added to the SVG canvas.
+   */
+  function line(x1, y1, x2, y2) {
+    var line = document.createElementNS(w3SvgUrl, 'line');
+
+    line.setAttributeNS(null, 'x1', x1);
+    line.setAttributeNS(null, 'y1', y1);
+    line.setAttributeNS(null, 'x2', x2);
+    line.setAttributeNS(null, 'y2', y2);
+
+    return line;
   }
 
   /**
@@ -168,6 +188,7 @@
     createSvg: createSvg,
     ellipse: ellipse,
     fill: fill,
+    line: line,
     rect: rect,
     stroke: stroke,
     square: square
