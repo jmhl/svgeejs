06fc23da8f7246698e9fdb4ad4f021dc2c43c574
James Haley <james.haley@zipongo.com>
2014-12-14 17:51:21 -0800

    Add tests for circle

--- test/circle.js
+++ test/circle.js
@@ -1,17 +1,27 @@
-var casper = require('casper');
 var SVGEE = require('../SVGEE');
-    // var canvas = document.getElementById('canvas');
-    // var svg = SVGEE.createSvg(500, 500);
-    // canvas.appendChild(svg);
-    // var circle = SVGEE.circle(100, 100, 100);
 
-casper.test.begin('test circle', 1, function suite(test) {
-  casper.start('http://localhost:8000', function() {
-    // test.assertExists('div.canvas', 'canvas exists');
-    this.echo(this.getTitle());
+var numOfTests = 4;
+
+casper.test.begin('testing circle', numOfTests, function(test) {
+  casper.start('http://localhost:8000/');
+
+  casper.thenOpen('http://localhost:8000/', function() {
+    test.assertExists('.test-circle', 'circle exists');
+
+    test.assertEvalEquals(function() {
+      return document.getElementsByClassName('test-circle')[0].getAttribute('r');
+    }, '40');
+
+    test.assertEvalEquals(function() {
+      return document.getElementsByClassName('test-circle')[0].getAttribute('cx');
+    }, '100');
+
+    test.assertEvalEquals(function() {
+      return document.getElementsByClassName('test-circle')[0].getAttribute('cy');
+    }, '100');
   });
 
   casper.run(function() {
     test.done();
-  });
+  })
 });
